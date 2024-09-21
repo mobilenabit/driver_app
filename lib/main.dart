5856d9abf043be8b061ca6084175a3e34dda9e46
GHCMaxim <maxim080503@gmail.com>
2024-09-21 16:18:52 +0700

    refactor: clean up imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:driver_app/app.dart';
 import 'package:driver_app/core/user_data.dart';
 import 'package:driver_app/models/licensePlate.dart';
-import 'package:driver_app/screens/account.dart';
 
-import 'package:driver_app/screens/home.dart';
-import 'package:driver_app/screens/info_test.dart';
-import 'package:driver_app/screens/login.dart';
-import 'package:driver_app/screens/news.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 void main() {
